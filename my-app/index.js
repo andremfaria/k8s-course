51f9093d52d85a3a8ed9b2d2b79ad6194ecbc4c9
andremfaria <andre.faria@appliedblockchain.com>
2024-06-12 19:50:39 +0100

    fix connection

--- my-app/index.js
+++ my-app/index.js
@@ -1,7 +1,6 @@
-const Koa = require('koa');
-const redis = require('redis');
+const Koa = require("koa");
+const redis = require("redis");
 const app = new Koa();
-
 
 const redisHost = process.env.REDIS_HOST;
 const redisPort = process.env.REDIS_PORT;
@@ -9,40 +8,26 @@ const redisPort = process.env.REDIS_PORT;
 console.log(`Connecting to Redis at ${redisHost}:${redisPort}`);
 
 const client = redis.createClient({
-  url: 'redis://redisHost:redisPort',
+  url: `redis://${redisHost}:${redisPort}`,
 });
-
+client.on("error", (err) => console.log("Redis Client Error", err));
+// Connect once at startup
+client.connect().then(() => {
+  console.log("Connected to Redis");
+}).catch(err => {
+  console.error("Error connecting to Redis", err);
+});
 const getAndIncrement = async () => {
-
-  return new Promise((resolve, reject) => {
-    client.get('counter', (err, value) => {
-      try {
-      if (err) {
-        reject(err);
-      } else {
-        const counter = parseInt(value) || 0;
-        const newCounter = counter + 1;
-        client.set('counter', newCounter, (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(newCounter);
-          }
-        });
-      }
-    }catch(err) {
-      console.error(err);
-      reject(err)
-    }
-    finally {
-      redisClient.disconnect()
-
-    }
-    });
-  });
-}
-app.use(async ctx => {
-  const counter = await getAndIncrement()
+  console.log("getAndIncrement");
+  try {
+    return  client.incr("new_count");;
+  } catch (err) {
+    console.error("error", err);
+    throw err;
+  } 
+};
+app.use(async (ctx) => {
+  const counter = await getAndIncrement();
   ctx.body = `Hello World! Counter is ${counter}`;
 });
 
